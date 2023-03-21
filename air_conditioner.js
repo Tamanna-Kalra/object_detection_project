e38043f5579247cef8c025b998d4fd096bf834dd
Tamanna-Kalra <83012226+Tamanna-Kalra@users.noreply.github.com>
2023-03-21 15:50:10 +0530

    Add files via upload

--- air_conditioner.js
+++ air_conditioner.js
@@ -1,16 +1,41 @@
 img = "";
+objects = [];
+status = "";
 function preload() {
     img = loadImage('air_conditioner.jpg');
 }
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, result) {
+    if (error){
+        console.log(error);
+    }
+    console.log(result);
+    objects = result;
 }
 function draw() {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Air conditioner", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "  " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect( objects[i].x , objects[i].y , objects[i].width , objects[i].height);
 }
+        }
+    }
+
